eb1775e8623ec7497ea68c3cd87c35293784340c
Bibek Adhikari <adhikaribibek84@gmail.com>
2025-06-17 23:58:37 +0545

    Initial commit

---
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.loginapp
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var usernameInput: EditText
+    lateinit var passwordInput: EditText
+    lateinit var loginButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginButton = findViewById(R.id.login_button)
+
+        loginButton.setOnClickListener {
+            var username = usernameInput.text.toString()
+            var password = passwordInput.text.toString()
+            Log.i("Test Credentials","Username: $username Password: $password")
+        }
+    }
+}
